a60340bfa976e78c8df39d93b604bfcb5e590dc5
Zahid Shams <zahid.cse02@gmail.com>
2018-09-25 17:39:47 +0600

    Migration Table Update

--- database/migrations/2018_09_25_113546_create_students3_table.php
+++ database/migrations/2018_09_25_113546_create_students3_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateStudents3Table extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('students3', function (Blueprint $table) {
+          $table->increments('id');
+            $table->string('name', 20);
+            $table->string('email',50);
+             $table->string('mobile',20);
+            $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students3');
     }
 }
